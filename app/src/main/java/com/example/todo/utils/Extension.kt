b65c162b9304679e057b1d5b76348168d3d336ab
ChoudharyAnil <anil.fauxiq@gmail.com>
2019-11-28 17:40:17 +0530

    - extension created for task

---
+++ app/src/main/java/com/example/todo/utils/Extension.kt
@@ -0,0 +1,26 @@
+package com.example.todo.utils
+
+import android.content.Context
+import android.widget.Toast
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import java.util.*
+
+fun Context.showToast(message:String){
+    Toast.makeText(this,message, Toast.LENGTH_LONG).show()
+}
+
+val currentDate: Long
+    get() {
+        return Calendar.getInstance().time.time
+    }
+
+fun <T> LiveData<T>.observeOnce(lifecycleOwner: LifecycleOwner, observer: Observer<T>) {
+    observe(lifecycleOwner, object : Observer<T> {
+        override fun onChanged(t: T?) {
+            observer.onChanged(t)
+            removeObserver(this)
+        }
+    })
+}
